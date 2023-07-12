c69d091ee230273062a2950f7f41ff2e85c2e696
eavedill <eavedillo@yahoo.es>
2023-07-12 22:28:35 +0200

    added function of replacement env. variable.

--- src/replace_env_var.c
+++ src/replace_env_var.c
@@ -11,19 +11,43 @@
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-/*
-void replace_env_var(t_instruct *inst, char *str)
+
+char	*replace_env_var(t_instruct *inst, char *str)
 {
-	int i;
-	int j;
+	int 	i;
+	int 	j;
+	char 	*variable;
+	char 	*value;
+	char 	*out;
 
 	while (str[++i])
 	{
-		if(str[i]== "$")
+		if (str[i] == "\'")
+			while (str[i] != "\'")
+				i++;
+		if (str[i] == '$')
 		{
 			j = i;
-			while (str)
+			while (str[j] != ' ')
+				j++;
+			variable = ft_subst_str(str, i + 1, j);
+			value = getenv(variable);
+			j = (ft_strlen(str) - ft_strle(variable) + ft_strlen(value) + 1);
+			out = (char *)malloc(j *sizeof(char));
+			out[j] = '\0';
+			j = 0
+			while (out[j])
+			{
+				if (j < i || j > i + ft_strlen(value))
+					out[j] = str[j];
+				else
+					out[j] = value[j - i];
+			}
+			free(str);
+			str = out;
+			out = NULL;
+			i += ft_strlen(value);
 		}
-			ft_subst_str()
 	}
-}*/
+	return (str);
+}
